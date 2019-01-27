18937c3ce28dd077c8314b7edda212650346a0cc
ksmolenwsei <krystian.smolen@microsoft.wsei.edu.pl>
2019-01-27 18:20:37 +0100

    dodanie projektu z testami jednostkowymi dla api połączenia z bazą danych

---
+++ UnitTestDatabaseAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDatabaseAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestDatabaseAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cff91872-62ef-45d6-bc90-2d4b25a8c088")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
