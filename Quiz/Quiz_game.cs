b5254e1bc85e2610b3db20f497c18405773ef825
ksmolenwsei <krystian.smolen@microsoft.wsei.edu.pl>
2019-02-06 18:48:56 +0100

    add XML documentation for quiz_game

--- Quiz/Quiz_game.cs
+++ Quiz/Quiz_game.cs
@@ -6,22 +6,37 @@ using DatabaseConnectionAPI;
 
 namespace QuizGameAPI
 {
+    /// <summary>
+    /// Klasa zawierająca logikę gry
+    /// </summary>
     public class QuizGame
     {
         private string Name;
         private int score = 0;
         private int counterQuestions = 1;
         private Database quizDatabase = new Database();
-
+        /// <summary>
+        /// Zwraca nazwęgracza
+        /// </summary>
+        /// <returns>string</returns>
         public string getName()
         {
             return Name;
         }
+        /// <summary>
+        /// pobiera nazwę gracza
+        /// </summary>
+        /// <param name="Name"></param>
         public void setName(string Name)
         {
             this.Name = Name;
         }
-
+        /// <summary>
+        /// Pobiera z bazy pytanie, jeśli metoda wyświetlila 5 pytań wywołuje metodę kończącą grę
+        /// </summary>
+        /// <param name="AllQuestion"></param>
+        /// <param name="Question"></param>
+        /// <param name="AnswerButtons"></param>
         public void Play(GroupBox AllQuestion, Label Question, List<Button> AnswerButtons)
         {
             quizDatabase.OpenConnection();
@@ -38,7 +53,11 @@ namespace QuizGameAPI
                 KoniecGry(AllQuestion);
             }
         }
-
+        /// <summary>
+        /// Przypisuje pytaia do odpowiednich elementów formularza
+        /// </summary>
+        /// <param name="answers"></param>
+        /// <param name="AnswerButtons"></param>
         private void setAnswers(List<Answers> answers, List<Button> AnswerButtons)
         {
             for (int i = 0; i < 4; i++)
@@ -48,7 +67,10 @@ namespace QuizGameAPI
             }
 
         }
-
+        /// <summary>
+        /// Kończy grę i dodaje wynik do bazy danych
+        /// </summary>
+        /// <param name="Question"></param>
         private void KoniecGry(GroupBox Question)
         {
             Question.Hide();
@@ -56,7 +78,13 @@ namespace QuizGameAPI
             quizDatabase.AddToLeaderboard(this.Name, this.score.ToString());
             counterQuestions = 1;
         }
-
+        /// <summary>
+        /// sprawdza czy odpowiedz jest poprawna oraz przypisuje punkty lub nie
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="AllQuestion"></param>
+        /// <param name="Question"></param>
+        /// <param name="AnswerButtons"></param>
         public void CheckAnswer(object value, GroupBox AllQuestion, Label Question, List<Button> AnswerButtons)
         {
             if (Convert.ToString(value) == "1")
